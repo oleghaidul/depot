2fda3376b48392b0a5ac65c50ef1f5a33440a938
Oleg Haidul <oleghaidul@gmail.com>
2012-01-14 18:32:53 +0300

    Add a session counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,11 @@
 class StoreController < ApplicationController
+	before_filter :counter
   def index
   	@products = Product.all
+  end
+
+  def counter
+  	@session = session[:counter] = session[:counter].is_a?(Fixnum) ? session[:counter] + 1 : 0
   end
 
 end
