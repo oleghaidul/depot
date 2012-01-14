98b284b3712fd325cb1607a295cdbd910d7936e4
Oleg Haidul <oleghaidul@gmail.com>
2012-01-14 14:17:09 +0300

    Add Formtastic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,25 @@ class ProductsController < ApplicationController
 		@products = Product.all
 	end
 
+	def new
+		@product = Product.new
+	end
+
+	 def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(params[:product])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+  end
+
 end
