47884e87b4ce53b27acdb3eba1d9be6bfcdeaf6f
Oleg Haidul <oleghaidul@gmail.com>
2012-01-14 18:36:00 +0300

    Playtime: reset counter to zero

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,7 +4,7 @@ class LineItemsController < ApplicationController
 		@cart = current_cart
 		product = Product.find(params[:product_id])
 		@line_item = @cart.line_items.build(:product => product)
-
+		session[:counter] = 0
 		if @line_item.save
 			redirect_to(@line_item.cart, :notice => 'Line item was successfully created.')
 		else
